d6c884f736673745a1046e7262505fe8b67df503
parjanyaacoder <padityashukla26@gmail.com>
2025-08-24 22:15:49 +0530

    fix - deploy frontend seperately

--- backend/src/app.js
+++ backend/src/app.js
@@ -153,17 +153,17 @@ io.on('connection', socket => {
 
 //console.log(path.join(__dirname,'../client/build', 'index.html'))
 
-if (process.env.NODE_ENV === 'production') {
+// if (process.env.NODE_ENV === 'production') {
 
 
 
-    // Serve any static files
-    app.use(express.static(path.join(__dirname,'../client/build')));
-  // Handle React routing, return all requests to React app
-    app.get('*', function(req, res) {
-      res.sendFile(path.join(__dirname,'../client/build', 'index.html'));
-     });
-  }
+//     // Serve any static files
+//     app.use(express.static(path.join(__dirname,'../client/build')));
+//   // Handle React routing, return all requests to React app
+//     app.get('*', function(req, res) {
+//       res.sendFile(path.join(__dirname,'../client/build', 'index.html'));
+//      });
+//   }
  
 
 
